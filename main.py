618a80bc553da131e96036d917c4b8423f88bd5a
fatou82 <fatoumatafole169@gmail.com>
2026-06-02 18:02:19 +0000

    premier commit local

---
+++ main.py
@@ -0,0 +1,26 @@
+''' Exemple 1: Tri de trois nombres dans l'ordre croissant '''
+def tri_croissant():
+    a = int(input("Entrez le premier nombre : "))
+    b = int(input("Entrez le deuxième nombre : "))
+    c = int(input("Entrez le troisième nombre : "))
+
+    if a <= b and a <= c:
+        print(a)
+        if b <= c:
+            print(b)
+            print(c)
+        else:
+            print(c)
+            print(b)
+tri_croissant()
+
+''' Exemple 2: conversion d'un nombre entier en binaire '''
+def chiffre_vers_binaire():
+    n = int(input("Entrez un nombre entier : "))
+    binaire = ""
+    while n > 0:
+        binaire = str(n % 2) + binaire
+        n = n // 2
+    print("Le nombre en binaire est :", binaire)
+
+chiffre_vers_binaire()
